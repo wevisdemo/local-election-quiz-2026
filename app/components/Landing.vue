033ce3b3594d8dee47154f26ca7403bfb8595030
keykunjaee <naanong.sa@gmail.com>
2025-12-11 13:01:46 +0700

    feat: add gif and adjust typography in landing page

--- app/components/Landing.vue
+++ app/components/Landing.vue
@@ -8,15 +8,15 @@ const emit = defineEmits<{
 
 <template>
   <div class="flex min-h-screen flex-col items-center justify-center px-6 text-center">
-    <div class="font-bold">
-      <p class="h8">เช็กความพร้อม</p>
-      <h5 class="relative z-20">ซ้อมความเป๊ะ</h5>
+    <div class="leading-7 font-bold">
+      <h6>เช็กความพร้อม</h6>
+      <h3 class="relative z-20 -translate-y-[2px]">ซ้อมความเป๊ะ</h3>
       <div class="flex items-center gap-2.5">
-        <p class="h8 relative z-20">ก่อนออกไปเลือกตั้ง</p>
+        <h6 class="relative z-20 md:mr-1">ก่อนออกไปเลือกตั้ง</h6>
         <div class="relative">
-          <h6 class="relative z-20">อบต.</h6>
+          <h4 class="relative z-20">อบต.</h4>
           <div
-            class="absolute top-1/2 left-1/2 z-10 h-[70px] w-[70px] flex-none -translate-x-1/2 -translate-y-1/2"
+            class="absolute top-1/2 left-1/2 z-10 h-20 w-20 flex-none -translate-x-1/2 -translate-y-1/2 md:h-[120px] md:w-[120px]"
           >
             <img class="h-full w-full" src="/assets/images/yellow-circle.svg" alt="" />
           </div>
@@ -24,12 +24,12 @@ const emit = defineEmits<{
       </div>
     </div>
 
-    <div class="py-10">
-      <img src="/assets/images/mock-landing-image.png" alt="" />
+    <div class="w-full max-w-[500px] py-10">
+      <img src="/gifs/landing.gif" alt="" />
     </div>
 
     <p class="b5 pb-1.5 font-bold">
-      11 มกรา เลือกตั้ง อบต. <br />
+      11 มกรา เลือกตั้ง อบต. <br class="md:hidden" />
       คุณพร้อมแล้วแค่ไหน?
     </p>
     <p class="b6 pb-2 font-medium">
